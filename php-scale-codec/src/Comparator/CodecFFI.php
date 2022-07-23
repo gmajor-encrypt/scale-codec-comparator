478c6ae9e464226c2028058e21eea093558e801e
gmajor <gmajorencrypt@gmail.com>
2022-07-23 22:21:32 +0800

    php ffi test

--- php-scale-codec/src/Comparator/CodecFFI.php
+++ php-scale-codec/src/Comparator/CodecFFI.php
@@ -20,5 +20,42 @@ class CodecFFI
         );
     }
 
+    public function CompactU32Encode (): string
+    {
+        $o = $this->FFIInstant->compact_u32_encode(2);
+        return FFI::string($o);
+    }
+
+    public function CompactU32Decode (): int
+    {
+        return $this->FFIInstant->compact_u32_decode("08");
+    }
+
+
+    public function OptionBoolEncode():string
+    {
+        $o = $this->FFIInstant->option_bool_encode("None");
+        return FFI::string($o);
+    }
+
+    public function OptionBoolDecode():string
+    {
+        $o = $this->FFIInstant->option_bool_decode("01");
+        return FFI::string($o);
+    }
+
+
+    public function BoolDecode():bool
+    {
+        return $this->FFIInstant->bool_decode("01");
+    }
+
+    public function BoolEncode():string
+    {
+        $o = $this->FFIInstant->bool_encode(true);
+        return FFI::string($o);
+    }
+
+
 }
 
--- php-scale-codec/test/Comparator/Test/BaseTest.php
+++ php-scale-codec/test/Comparator/Test/BaseTest.php
@@ -20,7 +20,14 @@ final class BaseTest extends TestCase
 
     public function testCodecCall ()
     {
+        $this->assertEquals("08", $this->FFICodec->CompactU32Encode());
+        $this->assertEquals(2, $this->FFICodec->CompactU32Decode());
 
+        $this->assertEquals("00", $this->FFICodec->OptionBoolEncode());
+        $this->assertEquals("true", $this->FFICodec->OptionBoolDecode());
+
+        $this->assertEquals(true, $this->FFICodec->BoolDecode());
+        $this->assertEquals(01, $this->FFICodec->BoolEncode());
     }
 
 }
