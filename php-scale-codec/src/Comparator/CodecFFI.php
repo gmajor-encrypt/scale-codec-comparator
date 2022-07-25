09535a4dc08aee5488e9b6087cde9599cc8c5b11
gmajor <gmajorencrypt@gmail.com>
2022-07-25 19:55:16 +0800

    php ffi test

--- php-scale-codec/src/Comparator/CodecFFI.php
+++ php-scale-codec/src/Comparator/CodecFFI.php
@@ -32,30 +32,74 @@ class CodecFFI
     }
 
 
-    public function OptionBoolEncode():string
+    public function OptionBoolEncode (): string
     {
         $o = $this->FFIInstant->option_bool_encode("None");
         return FFI::string($o);
     }
 
-    public function OptionBoolDecode():string
+    public function OptionBoolDecode (): string
     {
         $o = $this->FFIInstant->option_bool_decode("01");
         return FFI::string($o);
     }
 
 
-    public function BoolDecode():bool
+    public function BoolDecode (): bool
     {
         return $this->FFIInstant->bool_decode("01");
     }
 
-    public function BoolEncode():string
+    public function BoolEncode (): string
     {
         $o = $this->FFIInstant->bool_encode(true);
         return FFI::string($o);
     }
 
 
+    public function ResultEncode (): string
+    {
+        $o = $this->FFIInstant->results_encode(2, "NONE", "OK");
+        return FFI::string($o);
+    }
+
+    public function ResultDecode (): array
+    {
+        $o = $this->FFIInstant->results_decode("0002000000");
+        $ok = $o->ok;
+        $err = $o->err;
+        return ["OK" => $ok, "ERR" => FFI::string($err)];
+    }
+
+    public function StructEncode (): string
+    {
+        $tv = $this->FFIInstant->new("struct CodecStruct");
+        $tv->data = 10;
+        $tv->other = 1;
+        $o = $this->FFIInstant->data_struct_encode(FFI::addr($tv));
+        return FFI::string($o);
+    }
+
+    public function StructDecode (): array
+    {
+        $o = $this->FFIInstant->data_struct_decode("0a00000001");
+        $data = $o->data;
+        $other = $o->other;
+        return ["Data" => $data, "Other" => $other];
+    }
+
+    public function EnumEncode (): string
+    {
+        $tv = $this->FFIInstant->new("struct EnumStruct");
+        $tv->a = 1;
+        $o = $this->FFIInstant->data_enum_encode(FFI::addr($tv));
+        return FFI::string($o);
+    }
+
+    public function EnumDecode (): array
+    {
+        $o = $this->FFIInstant->data_enum_decode("0001000000");
+        return ["a" => $o->a, "b" => $o->b, "c" => $o->c];
+    }
 }
 
--- php-scale-codec/test/Comparator/Test/BaseTest.php
+++ php-scale-codec/test/Comparator/Test/BaseTest.php
@@ -28,6 +28,20 @@ final class BaseTest extends TestCase
 
         $this->assertEquals(true, $this->FFICodec->BoolDecode());
         $this->assertEquals(01, $this->FFICodec->BoolEncode());
+
+        $this->assertEquals("0002000000", $this->FFICodec->ResultEncode());
+        $this->assertEquals(["OK" => 2, "ERR" => ""], $this->FFICodec->ResultDecode());
+
+
+        $this->assertEquals(["Data" => 10, "Other" => 1], $this->FFICodec->StructDecode());
+        $this->assertEquals("0a00000001", $this->FFICodec->StructEncode());
+
+
+        $this->assertEquals(["a" => 1, "b" => 0,"c"=>0], $this->FFICodec->EnumDecode());
+        $this->assertEquals("0001000000", $this->FFICodec->EnumEncode());
+
+
     }
+
 
 }
