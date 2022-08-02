91ca3be4b535ba2f912ba1fd8c2ef70d75182b55
gmajor <gmajorencrypt@gmail.com>
2022-08-02 22:10:10 +0800

    python ffi support

--- py-scale-codec/scale-codec-comparator/codec.py
+++ py-scale-codec/scale-codec-comparator/codec.py
@@ -89,8 +89,54 @@ struct TupleType*  tuple_u32u32_decode(char* raw);
     def bool_encode(self, boolValue):
         return self.to_utf8(self.lib.bool_encode(boolValue))
 
+    def results_encode(self, uint32):
+        return self.to_utf8(
+            self.lib.results_encode(uint32, self.str_to_ffi_string("None"), self.str_to_ffi_string("OK")))
 
+    def results_decode(self, raw):
+        raw = self.str_to_ffi_string(raw)
+        return self.lib.results_decode(raw)
 
+    def struct_encode(self, st):
+        return self.to_utf8(self.lib.data_struct_encode(st))
+
+    def struct_decode(self, raw):
+        raw = self.str_to_ffi_string(raw)
+        return self.lib.data_struct_decode(raw)
+
+    def enum_encode(self, st):
+        return self.to_utf8(self.lib.data_enum_encode(st))
+
+    def enum_decode(self, raw):
+        raw = self.str_to_ffi_string(raw)
+        return self.lib.data_enum_decode(raw)
+
+    def string_decode(self, raw):
+        return self.lib.string_decode(self.str_to_ffi_string(raw))
+
+    def string_encode(self, boolValue):
+        return self.to_utf8(self.lib.string_encode(boolValue))
+
+    def tuple_encode(self, st):
+        return self.to_utf8(self.lib.tuple_u32u32_encode(st))
+
+    def tuple_decode(self, raw):
+        raw = self.str_to_ffi_string(raw)
+        return self.lib.tuple_u32u32_decode(raw)
+
+    def fixed_u32_encode(self, arr):
+        return self.to_utf8(self.lib.fixU32_encode(arr, len(arr)))
+
+    def fixed_u32_decode(self, raw):
+        raw = self.str_to_ffi_string(raw)
+        return self.lib.fixU32_decode(raw)
+
+    def vec_u32_encode(self, arr):
+        return self.to_utf8(self.lib.vec_u32_encode(arr, len(arr)))
+
+    def vec_u32_decode(self, raw):
+        raw = self.str_to_ffi_string(raw)
+        return self.lib.vec_u32_decode(raw)
 
     def to_utf8(self, raw):
         return str(self.ffi.string(raw), 'utf-8')
@@ -101,3 +147,43 @@ struct TupleType*  tuple_u32u32_decode(char* raw);
 
 if __name__ == '__main__':
     c = Codec()
+    # result = c.results_decode("0002000000")
+    # print(result.ok, c.to_utf8(result.err))
+    # s1 = c.ffi.new("struct CodecStruct *")
+    # s1.data = 10
+    # s1.other = 1
+    # print(c.struct_encode(s1))
+    #
+    # s2 = c.struct_decode("0a00000001")
+    # print(s2.data)
+    # s1 = c.ffi.new("struct EnumStruct *")
+    # s1.a = 1
+    # print(c.enumEncode(s1))
+    # s2 = c.enumdecode("0001000000")
+    # print(s2.a)
+
+    # s1 = c.ffi.new("struct TupleType *")
+    # s1.a = 10
+    # s1.b = 1
+    # print(c.tuple_encode(s1))
+    # s2 = c.tuple_decode("0a00000001000000")
+    # print(s2.a)
+    # print(c.fixed_u32_encode([1, 2, 3, 4, 5, 6]))
+    # s3 = c.fixed_u32_decode("010000000200000003000000040000000500000006000000")
+    # my_python_list = c.ffi.unpack(s3, 6)
+    # print(my_python_list)
+    # int_array = c.ffi.new("unsigned int[6]")
+    # buf = c.ffi.memmove(int_array, s3,24)
+    # print(int_array)
+    # print(int_array[1])
+    # print(int_array[2])
+    # print(int_array[3])
+    # s4 = []
+    # array = c.ffi.new("unsigned int[6]")
+    # print("size",c.ffi.sizeof(s3))
+    # print(c.ffi.from_buffer("unsigned int[6]", s3))
+    # print(array[0])
+    # c.ffi.addressof(s3)
+    # for x in range(6):
+    # print(x)
+    # print(int_array[x])
