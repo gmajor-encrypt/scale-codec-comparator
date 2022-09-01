dc40b3de2a2ed770dee9b7ae00603cd2f6017003
gmajor <gmajorencrypt@gmail.com>
2022-09-01 12:03:48 +0800

    support scale.rb test

--- scale.rb/spec/base_spec.rb
+++ scale.rb/spec/base_spec.rb
@@ -150,63 +150,63 @@ RSpec.describe Scale::Types do
     expect(Rust.bool_decode("01")).to eq(Scale::Types::Bool.decode(Scale::Bytes.new("0x01")).value)
     expect(Rust.bool_encode(true)).to eq(Scale::Types::Bool.new(true).encode)
     #
-    # results
-    # expect(Rust.results_encode(2,"None","OK")).to eq("0002000000")
-    # results_output = Rust.results_decode("0002000000")
-    # expect(:Ok=>results_output[:ok],:Err=>results_output[:err]).to eq({:Ok=>2,:Err=>""})
-    #
+
     # # struct
-    # struct_output = Rust.data_struct_decode("0a00000001")
-    # expect(:Data=>struct_output[:data],:Other=>struct_output[:other]).to eq({:Data=>10,:Other=>1})
-    # cs = CodecStruct.new
-    # cs[:data] = 10
-    # cs[:other] = 1
-    # expect(Rust.data_struct_encode(cs)).to eq("0a00000001")
     #
-    #
-    # # enum
-    # enum_output = Rust.data_enum_decode("0001000000")
-    # expect(:a=>enum_output[:a],:b=>enum_output[:b],:c=>enum_output[:c]).to eq({:a=>1,:b=>0,:c=>0})
-    # cs = EnumStruct.new
-    # cs[:a] = 1
-    # expect(Rust.data_enum_encode(cs)).to eq("0001000000")
-    #
-    # # string
-    # expect(Rust.string_encode("Hamlet")).to eq("1848616d6c6574")
-    # expect(Rust.string_decode("1848616d6c6574")).to eq("Hamlet")
-    #
-    #
-    # # tuple
-    # tuple_output = Rust.tuple_u32u32_decode("0a00000001000000")
-    # expect(:a=>tuple_output[:a],:b=>tuple_output[:b]).to eq({:a=>10,:b=>1})
-    # cs = TupleType.new
-    # cs[:a] = 10
-    # cs[:b] = 1
-    # expect(Rust.tuple_u32u32_encode(cs)).to eq("0a00000001000000")
-    #
-    # # fixed uint32
-    # fixed_arr = [1, 2, 3, 4, 5, 6]
-    # size = fixed_arr.size
-    # ptr = FFI::MemoryPointer.new :uint32, size
-    # ptr.put_array_of_uint32 0, fixed_arr
-    # expect(Rust.fixU32_encode(ptr,size)).to eq("010000000200000003000000040000000500000006000000")
-    #
-    # fix_u32_output =  Rust.fixU32_decode("010000000200000003000000040000000500000006000000")
-    # result_array = fix_u32_output.read_array_of_uint32(size)
-    # expect(result_array).to eq( [1, 2, 3, 4, 5, 6])
-    # # vec<u32>
-    # vec_u32 = [1, 2, 3, 4, 5, 6]
-    # size = vec_u32.size
-    # ptr_vec = FFI::MemoryPointer.new :uint32, size
-    # ptr_vec.put_array_of_uint32 0, fixed_arr
-    # expect(Rust.vec_u32_encode(ptr_vec,size)).to eq("18010000000200000003000000040000000500000006000000")
-    #
-    # vec_u32_output =  Rust.vec_u32_decode("18010000000200000003000000040000000500000006000000")
-    # result_array = vec_u32_output.read_array_of_uint32(size)
-    # expect(result_array).to eq( [1, 2, 3, 4, 5, 6])
+    struct_type = { "type" => "struct", "type_mapping" => [["Data", "u32"], ["Other", "U8"]] }
+    codec_struct = Scale::Types.get(struct_type)
+    struct_output = Rust.data_struct_decode("0a00000001")
+
+    expect({"Data"=>struct_output[:data],"Other"=>struct_output[:other]}).to eq(codec_struct.decode(Scale::Bytes.new("0x0a00000001")).value.map{|k,v| [k, v.value]}.to_h)
+    cs = CodecStruct.new
+    cs[:data] = 10
+    cs[:other] = 1
+    expect(Rust.data_struct_encode(cs)).to eq(codec_struct.new({"Data"=>Scale::Types::U32.new(10),"Other"=>Scale::Types::U8.new(1)}).encode)
 
 
+    # tuple
+    tuple_output = Rust.tuple_u32u32_decode("0a00000001000000")
+    expect([tuple_output[:a],tuple_output[:b]]).to eq(Scale::Types.get("(U32, U32)").decode(Scale::Bytes.new("0x0a00000001000000")).value.map{|v| v.value})
+    cs = TupleType.new
+    cs[:a] = 10
+    cs[:b] = 1
+    expect(Rust.tuple_u32u32_encode(cs)).to eq(Scale::Types.get("(U32, U32)").new([Scale::Types::U32.new(10),Scale::Types::U32.new(1)]).encode)
+
+    # vec<u32>
+    vec_u32 = [1, 2, 3, 4, 5, 6]
+    size = vec_u32.size
+    ptr_vec = FFI::MemoryPointer.new :uint32, size
+    ptr_vec.put_array_of_uint32 0, fixed_arr
+    expect(Rust.vec_u32_encode(ptr_vec,size)).to eq(Scale::Types.get("Vec<U8>").new(vec_u32).encode)
+
+    vec_u32_output =  Rust.vec_u32_decode("18010000000200000003000000040000000500000006000000")
+    result_array = vec_u32_output.read_array_of_uint32(size)
+    expect(result_array).to eq( Scale::Types.get("Vec<U8>").decode(Scale::Bytes.new("0x18010000000200000003000000040000000500000006000000")))
+  end
+  #
+  it "FFI codec output compare with scala.rb, result types should support" do
+     Scale::Types.get("Results").nil?
   end
 
+
+  it "FFI codec output compare with scala.rb, enum types should support" do
+    enum_type = { "type" => "enum", "type_mapping" => [["a", "u32"], ["b", "u32"],["c","u32"]] }
+    codec_enum = Scale::Types.get(enum_type)
+    enum_output = Rust.data_enum_decode("0001000000")
+    expect(:a=>enum_output[:a]).to eq(codec_enum.decode(Scale::Bytes.new("0x0001000000")))
+    cs = EnumStruct.new
+    cs[:a] = 1
+    value = codec_enum.new({"a"=>Scale::Types::U32.new(1)})
+    expect(Rust.data_enum_encode(cs)).to eq(value.encode)
+  end
+
+  it "FFI codec output compare with scala.rb, string types should support" do
+    expect(Rust.string_decode("1848616d6c6574")).to eq(Scale::Types::String.decode(Scale::Bytes.new("0x1848616d6c6574")).value)
+    expect(Rust.string_encode("Hamlet")).to eq(Scale::Types::String.new("Hamlet").encode)
+  end
+
+  it "FFI codec output compare with scala.rb, fixed array types should support" do
+    Scale::Types.get("VecU32Length6").nil?
+  end
 
 end
