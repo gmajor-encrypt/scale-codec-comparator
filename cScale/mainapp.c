38289cf12f6118e9dabef8944d1ffa18ea6958dc
gmajor <gmajorencrypt@gmail.com>
2022-09-19 16:52:48 +0800

    cscale use ffi

--- cScale/mainapp.c
+++ cScale/mainapp.c
@@ -1,33 +1,34 @@
 #include <stdio.h>
 #include "cScale/src/scale.h"
 #include "../src/scale_ffi.h"
+#include <assert.h>
+#include <string.h>
+
 
 int main()
 {
-    printf("%s", compact_u32_encode(1));
+//    printf("%s", compact_u32_encode(1));
+    assert(strcasecmp(compact_u32_encode(1), "04") == 0);
+    assert(compact_u32_decode("04")== 1);
 
-//    scale_fixed_int fixed = { 0 };
-//    encode_int_to_fixed_int_scale(&fixed, (uint16_t)42);
-//    uint8_t serialized[64] = { 0 };
-//    size_t serialized_len = 0;
-//    serialize_fixed_int(serialized, &serialized_len, &fixed);
-//
-//    uint16_t output = 0;
-//    decode_scale_fixed_int((void*)&output, &fixed);
-//
-//    for(int i=0; i < serialized_len; i++) printf("%02X", serialized[i]);
-//    printf(" --- %u\n", output);
-//
-//
-//    scale_compact_int compact = SCALE_COMPACT_INT_INIT;
-//    encode_compact(&compact, (uint32_t)69);
-//    uint8_t serialized[64] = { 0 };
-//    size_t serialized_len = 0;
-//    char *output = decode_compact_to_hex(&compact);
-//    serialize_compact_int(serialized, &serialized_len, &compact);
-//    uint32_t decoded = strtoul(output, NULL, 16);
-//    printf("SCALE=<");
-//    for(int i=0; i < serialized_len; i++) printf("%02X", serialized[i]);
-//    printf("> --- Hex=<%s> --- Decoded=<%u>\n", output, decoded);
-//    free(output);
+    assert(strcasecmp(option_bool_encode("None"), "00") == 0);
+    assert(strcasecmp(option_bool_decode("01"), "true") == 0);
+
+    assert(strcasecmp(bool_encode(true), "01") == 0);
+    assert(bool_decode("01") == true);
+
+    struct ResultsType resultTest;
+    resultTest.ok= 2;
+    resultTest.err= "";
+    assert(strcasecmp(results_encode(&resultTest), "0002000000") == 0);
+
+    struct ResultsType resultDecodeRaw = *results_decode("0002000000");
+    assert(resultDecodeRaw.ok==2);
+
+    assert(strcasecmp(string_encode("Hamlet"), "1848616d6c6574") == 0);
+
+    uint32_t values[6] = { 1,2,3,4,5,6 };
+    assert(strcasecmp(fixU32_encode(values,6), "010000000200000003000000040000000500000006000000") == 0);
+
+    assert(strcasecmp(vec_u32_encode(values,6), "18010000000200000003000000040000000500000006000000") == 0);
 }
