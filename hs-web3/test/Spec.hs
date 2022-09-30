f1a719784dfacc98fd2177637c6343323fe07b67
gmajor <gmajorencrypt@gmail.com>
2022-09-30 12:40:25 +0800

    hs-web3 init

---
+++ hs-web3/test/Spec.hs
@@ -0,0 +1,32 @@
+{-# LANGUAGE DeriveAnyClass      #-}
+{-# LANGUAGE DeriveGeneric       #-}
+{-# LANGUAGE OverloadedStrings   #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+
+
+module Main where
+
+import           Control.Monad         (forM_)
+import           Data.Bit              (castFromWords8)
+import           Data.Bits             (bit)
+import           Data.ByteString       (ByteString)
+import qualified Data.ByteString       as BS (length, pack, unpack)
+import           Data.Int              (Int16, Int32, Int64, Int8)
+import qualified Data.Text             as T (pack)
+import           Data.Vector.Unboxed   (Vector)
+import qualified Data.Vector.Unboxed   as V (fromList)
+import           Data.Word             (Word16, Word32, Word64, Word8)
+import qualified GHC.Generics          as GHC (Generic)
+import           Codec.Scale
+
+
+data EnumType = A
+    | B Word32 Word64
+    | C
+    { a2 :: Word32
+    , b2 :: Word64
+    }
+    deriving (Eq, Show, GHC.Generic, Generic, Encode, Decode)
+
+main :: IO ()
+main = putStrLn "Test suite not yet implemented"
